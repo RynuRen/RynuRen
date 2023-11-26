248a973b34650d9784a726b99fa091b6e6badf07
RynuRen <pros0327@gmail.com>
2023-11-26 16:42:31 +0900

    Fix try

--- build-svg.js
+++ build-svg.js
@@ -7,26 +7,26 @@ import qty from 'js-quantities'
 const weather = await new AsyncWeather()
 
 const emojis = {
-  '01d': '☀️',
-  '02d': '⛅️',
-  '03d': '☁️',
-  '04d': '☁️',
-  '09d': '🌧',
-  '10d': '🌦',
-  '11d': '🌩',
-  '13d': '❄️',
-  '50d': '🌫'
+    '01d': '☀️',
+    '02d': '⛅️',
+    '03d': '☁️',
+    '04d': '☁️',
+    '09d': '🌧',
+    '10d': '🌦',
+    '11d': '🌩',
+    '13d': '❄️',
+    '50d': '🌫'
 }
 
 // Time working at PlanetScale
 function convertTZ(date, tzString) {
-    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", { timeZone: tzString }));
 }
 const today = convertTZ(new Date(), "Asia/Seoul");
 const todayDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(today);
 
 const psTime = formatDistance(new Date(2020, 12, 14), today, {
-  addSuffix: false
+    addSuffix: false
 })
 
 // Today's weather
@@ -36,30 +36,29 @@ weather.setUnits('imperial')
 weather.setApiKey(OPENWEATHER_API_KEY)
 
 weather.getAllWeather(function (err, data) {
-  if (err) console.log(err)
+    if (err) console.log(err)
 
-  const degF = Math.round(data.main.temp_max)
-  const degC = Math.round(qty(`${degF} tempF`).to('tempC').scalar)
-  const icon = data.weather[0].icon
+    const degF = Math.round(data.main.temp_max)
+    const degC = Math.round(qty(`${degF} tempF`).to('tempC').scalar)
+    const icon = data.weather[0].icon
 
-  fs.readFile('template.svg', 'utf-8', (error, data) => {
-    if (error) {
-      console.error(error)
-      return
-    }
+    fs.readFile('template.svg', 'utf-8', (error, data) => {
+        if (error) {
+            console.error(error)
+            return
+        }
 
-    data = data.replace('{degF}', degF)
-    data = data.replace('{degC}', degC)
-    data = data.replace('{weatherEmoji}', emojis[icon])
-    data = data.replace('{psTime}', psTime)
-    data = data.replace('{todayDay}', todayDay)
+        data = data.replace('{degF}', degF)
+        data = data.replace('{degC}', degC)
+        data = data.replace('{weatherEmoji}', emojis[icon])
+        data = data.replace('{psTime}', psTime)
+        data = data.replace('{todayDay}', todayDay)
 
-    data = fs.writeFile('dist/chat.svg', data, (err) => {
-      if (err) {
-        console.error(err)
-        return
-    } else {
-        console.log("Create Success")
-    }})
-  })
+        data = fs.writeFile('chat.svg', data, (err) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
+    })
 })
