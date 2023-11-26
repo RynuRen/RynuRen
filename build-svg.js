31110f3e948eb225b2d04e3a524aee60b3cacb30
RynuRen <pros0327@gmail.com>
2023-11-26 15:16:33 +0900

    Fix dependencies

--- build-svg.js
+++ build-svg.js
@@ -1,10 +1,9 @@
 const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY
 
-import { AsyncWeather } from '@cicciosgamino/openweather-apis'
 let fs = require('fs')
 let formatDistance = require('date-fns/formatDistance')
+let weather = require('@cicciosgamino/openweather-apis')
 let qty = require('js-quantities')
-const weather = await new AsyncWeather
 
 const emojis = {
   '01d': '☀️',
